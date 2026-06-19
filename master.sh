39fc73c56e5066d319ae18f1c3ca8b3452296a15
MrBarszcz <marcosbarszcz@gmail.com>
2026-06-19 18:55:06 -0300

    feat: adds orchestration of mysql and rabbitmq via docker stack

--- master.sh
+++ master.sh
@@ -3,3 +3,6 @@ sudo docker swarm init --advertise-addr=192.168.56.11
 
 echo "=== Gerando o script de join para os workers ==="
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+
+echo "=== Realizando o Deploy Automático do Banco e Mensageria ==="
+sudo docker stack deploy -c /vagrant/docker-compose.yaml infra
