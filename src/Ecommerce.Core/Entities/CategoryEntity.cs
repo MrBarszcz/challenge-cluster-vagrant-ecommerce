7545e1d566be7efc2cdd8cd65315660bf03ab18e
MrBarszcz <marcosbarszcz@gmail.com>
2026-06-22 13:22:02 -0300

    feat: adds category and platform domain entities

---
+++ src/Ecommerce.Core/Entities/CategoryEntity.cs
@@ -0,0 +1,7 @@
+namespace Ecommerce.Core.Entities {
+    public class CategoryEntity {
+        public Guid Id { get; private set; }
+        public string? category { get; private set; }   
+    }
+}
+
---
+++ src/Ecommerce.Core/Entities/PlatformEntity.cs
@@ -0,0 +1,6 @@
+namespace Ecommerce.Core.Entities;
+
+public class PlatformEntity {
+    public Guid Id { get; private set; }
+    public string? platform { get; private set; }
+}
