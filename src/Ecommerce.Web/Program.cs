109cc4efd8a06e9d796c706314084f958fb45031
MrBarszcz <marcosbarszcz@gmail.com>
2026-06-27 17:33:17 -0300

    feat: adds catalog view and sets it as default route

--- src/Ecommerce.Web/Program.cs
+++ src/Ecommerce.Web/Program.cs
@@ -42,8 +42,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Catalog}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
